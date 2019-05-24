422e740fce4f3d0fc63eb991caf15d98eb83618d
soonju <poodle0403@likelion.org>
2019-05-24 17:43:20 +0900

    first

---
+++ second/blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Blog
+
+def home(request):
+    blogs = Blog.objects # 클래스이름.objects쓰면 models.py로부터 객체 목록을 전달받을 수 있게 됨 
+                         # 쿼리셋 = models.py로부터 전달받은 객체 목록
+                         # 메소드 = 쿼리셋을 이용해 데이터베이스로부터 받은 기능들을 표시해줌
+                         #       = 쿼리셋 활용할 수 있게 함 (쿼리셋 만들고, 지우고 등등)
+                         # 메소드 형식은 -> 모델이름.쿼리셋 -> objects.메소드
+                         # home.html에서 blogs.all에는 내가 작성한 모든 블로그 글들이 담김 (.all도 메소드)
+    return render(request, 'home.html', {'blogs' : blogs})
+
+
+def detail(request, blog_id):
+        blog_detail = get_object_or_404(Blog, pk = blog_id)   # (어떤 클래스로부터, 몇 번 인자를 받을 지)
+        return render(request, 'detail.html', {'blog': blog_detail})
+
+def new(request): # new.html을 띄워주는 함수
+    return render(request, 'new.html')
+
+def create(request):   # 입력받은 내용을 데이터베이스에 넣어주는 함수
+    blog = Blog()   #Blog 클래스로부터 blog객체 생성 -> blog객체 안에는 title, body, pub_date 변수 있음
+    blog.title = request.GET['title']  #new.html에서 만든 name = "title"에 해당하는 내용 가져와서 안에 담음
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save() # .save()는 쿼리셋메소드중 하나임, blog라는 객체에 title 변수에 넣은 내용, 
+                # body에 넣은 내용, pub_date에 넣은 내용을 데이터베이스에 저장해라 라는 메소드이다.
+    return redirect('/blog/' +str(blog.id)) #blog.id는 int형이므로 문자열로 바뀌주기 위해 str 붙여줌
+ 
